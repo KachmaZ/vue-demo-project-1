e2c66cdeaf0b09b8e5401e50858b04bba5add63a
Arthur Kachmazov <boss.kachmaz@mail.ru>
2022-02-19 02:44:39 +0300

    Project created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CustomRange />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CustomRange from "@/components/CustomRange.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    CustomRange,
   },
 };
 </script>
