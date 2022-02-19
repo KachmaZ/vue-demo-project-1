d712d9c78ab0aa59ce6081a51446bde2a3874ae0
Arthur Kachmazov <boss.kachmaz@mail.ru>
2022-02-19 03:21:35 +0300

    Main page links config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <router-link to="/range">To Range</router-link>
-    <router-link to="/users">To Users</router-link>
+    <div class="link-menu">
+      <router-link class="link-tyle" to="/range">To Range</router-link>
+      <router-link class="link-tyle" to="/users">To User</router-link>
+    </div>
   </div>
 </template>
 
@@ -12,3 +14,31 @@ export default {
   name: "Home",
 };
 </script>
+
+<style lang="scss" scoped>
+.link-menu {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+
+  .link-tyle {
+    width: 50%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    text-decoration: none;
+    color: rgb(208, 211, 38);
+    font-size: 72px;
+
+    transition: 0.5s ;
+
+    &:hover {
+      background-color: #333b66;
+    }
+
+  }
+}
+</style>
