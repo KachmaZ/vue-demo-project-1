9d2a6805185a08f9e7c3a8723619c18ceac25090
Arthur Kachmazov <boss.kachmaz@mail.ru>
2022-02-21 03:00:29 +0300

    Incapsulating main page content to separate comp.

---
+++ src/components/HomeStaff.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home-staff">
+    <div class="link-menu">
+      <router-link class="link-tyle" to="/range">To Range</router-link>
+      <router-link class="link-tyle" to="/users">To User</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapActions} from "vuex"
+
+export default {    
+  methods: mapActions(["fetchAll"]),
+
+  async mounted() {
+    this.fetchAll();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.link-menu {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+
+  .link-tyle {
+    width: 50%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    text-decoration: none;
+    color: #d0d326;
+    font-size: 72px;
+
+    background: #1e233c;
+
+    transition: 0.5s;
+
+    &:hover {
+      background-color: #333b66;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,17 @@
 <template>
   <div class="home">
-    <div class="link-menu">
-      <router-link class="link-tyle" to="/range">To Range</router-link>
-      <router-link class="link-tyle" to="/users">To User</router-link>
-    </div>
+    <HomeStaff />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HomeStaff from "@/components/HomeStaff";
 
 export default {
   name: "Home",
+  components: {
+    HomeStaff,
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-.link-menu {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-
-  .link-tyle {
-    width: 50%;
-    height: 100%;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    text-decoration: none;
-    color: rgb(208, 211, 38);
-    font-size: 72px;
-
-    background: #1e233c;
-
-    transition: 0.5s;
-
-    &:hover {
-      background-color: #333b66;
-    }
-  }
-}
-</style>
