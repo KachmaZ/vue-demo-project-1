e83c5627b1242589a747c27776c3957b7a304bed
Arthur Kachmazov <boss.kachmaz@mail.ru>
2023-02-08 18:21:16 +0300

    Header width  100%

--- src/components/RangeHeader.vue
+++ src/components/RangeHeader.vue
@@ -1,31 +1,33 @@
 <template>
-    <div class="range-header container">
-      <ToMainLink />
-    </div>
-  </template>
+  <div class="range-header container">
+    <ToMainLink />
+  </div>
+</template>
   
   <script>
-  import ToMainLink from "./ToMainLink.vue";
-  
-  export default {
-    components: {
-      ToMainLink,
-    },
-  };
-  </script>
+import ToMainLink from "./ToMainLink.vue";
+
+export default {
+  components: {
+    ToMainLink,
+  },
+};
+</script>
   
   <style lang="scss" scoped>
-  .range-header {
-    padding: 10px;
+.range-header {
+  width: 100%;
   
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 10;
-  
-    display: flex;
-    align-items: center;
-  
-    background: #1e233c;
-  }
-  </style>
+  padding: 10px;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+
+  display: flex;
+  align-items: center;
+
+  background: #1e233c;
+}
+</style>
--- src/components/UsersGridHeader.vue
+++ src/components/UsersGridHeader.vue
@@ -22,6 +22,7 @@ export default {
 
 <style lang="scss" scoped>
 .users-grid-header {
+  width: 100%;
   padding: 10px;
 
   position: fixed;
